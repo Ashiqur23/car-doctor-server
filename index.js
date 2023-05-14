6d5f16f2b9da6228b9c7db137ed571ae513252a7
Ashiqur Rahman Biplop <biplops22@gmail.com>
2023-05-14 13:17:00 +0600

    added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
@@ -24,6 +25,22 @@ const client = new MongoClient(uri, {
   },
 });
 
+const verifyJwt = (req, res, next) => {
+  const authorization = req?.headers?.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: "unAuthorized User" });
+  }
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKET_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unAuthorized User" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 async function run() {
   const serviceCollection = client.db("carDoctorDb").collection("services");
 
@@ -33,6 +50,18 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    // JWT
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      // console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKET_SECRET, {
+        expiresIn: '1h',
+      });
+      // console.log({ token });
+      res.send({ token });
+    });
+
+    // services
     app.get("/services", async (req, res) => {
       const result = await serviceCollection.find().toArray();
       res.send(result);
@@ -48,22 +77,54 @@ async function run() {
       const result = await serviceCollection.findOne(filter, options);
       res.send(result);
     });
-
-    // booking
+    // post booking
     app.post("/bookings", async (req, res) => {
       const booking = req.body;
       const result = await bookingCollection.insertOne(booking);
       res.send(result);
     });
     //get booking
-    app.get("/bookings", async (req, res) => {
-      // console.log(req.query.email);
-      let query = {};
-      if(req?.query?.email){
-        query = {email: req?.query?.email}
+    app.get("/bookings", verifyJwt, async (req, res) => {
+      const decoded = req.decoded;
+      console.log(decoded);
+      if (decoded?.email !== req.query.email) {
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden access" });
       }
-      console.log(query);
+      // console.log(req.headers.authorization);
+      let query = {};
+      if (req?.query?.email) {
+        query = { email: req?.query?.email };
+      }
+      // console.log(query);
       const result = await bookingCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.put("/bookings/:id", async (req, res) => {
+      const id = req?.params?.id;
+      const body = req?.body;
+      const query = { _id: new ObjectId(id) };
+      const updateData = {
+        $set: {
+          status: body?.status,
+        },
+      };
+      const options = { upsert: true };
+
+      const result = await bookingCollection.updateOne(
+        query,
+        updateData,
+        options
+      );
+      res.send(result);
+    });
+
+    app.delete("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await bookingCollection.deleteOne(filter);
       res.send(result);
     });
 
